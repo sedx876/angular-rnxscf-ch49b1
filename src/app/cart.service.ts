84c78caedf5df9d5bebf842811db64f473bb019e
sedx876 <swatkins876@gmail.com>
2021-05-07 17:44:16 -0400

    3 files modified

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -4,5 +4,19 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class CartService {
+  items = [];
+
+  addToCart(product) {
+    this.items.push(product);
+  }
+
+  getItems() {
+    return this.items;
+  }
+
+  clearCart() {
+    this.items = [];
+    return this.items;
+  }
   constructor() {}
 }
